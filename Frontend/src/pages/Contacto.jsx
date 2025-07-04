4cebee18ec7b7f41a6bcf1153402c6c3f7eeb61c
JoaquinC2000 <joaquincenicero2000@hotmail.com>
2025-07-03 23:40:58 -0300

    diseño mínimo de la Landing page

---
+++ Frontend/src/pages/Contacto.jsx
@@ -0,0 +1,7 @@
+export default function Contacto() {
+    return (
+        <section id='contacto' className="h-screen">
+            <h2 className="text-3xl font-bold mb-10">Contacto</h2>
+        </section>
+    );
+}
---
+++ Frontend/src/pages/Inicio.jsx
@@ -0,0 +1,37 @@
+import portada from "../assets/portada.jpg"; // Imagen de fondo (corredor)
+
+export default function Hero() {
+  return (
+    <section id='inicio' className="h-screen">
+      <div
+        className="relative h-screen bg-cover bg-center flex items-center justify-start text-white"
+        style={{ backgroundImage: `url(${portada})` }}
+      >
+        <div className="p-10 max-w-xl">
+          <h1 className="text-5xl font-bold mt-8 mb-4">
+            RUNNING
+          </h1>
+
+          <hr className="w-32 border-green-400 border-t-4 my-4" />
+          <p className="text-sm">
+            Unete a nuestra comunidad de corredores y descubre tu potencial...
+          </p>
+          <div className="flex gap-6 mt-6">
+            <div>
+              <p className="text-green-400 text-xl font-bold">40+</p>
+              <p className="text-xs">Corredores activos</p>
+            </div>
+            <div>
+              <p className="text-green-400 text-xl font-bold">1+</p>
+              <p className="text-xs">Año de experiencia</p>
+            </div>
+            <div>
+              <p className="text-green-400 text-xl font-bold">10+</p>
+              <p className="text-xs">Competencias nacionales</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
